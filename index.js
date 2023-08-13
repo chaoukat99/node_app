4b7c3a44e95d4048eefa259f1a3a304267258b45
chaoukat99 <chawkatomar@gmail.com>
2023-08-13 23:04:52 +0200

    F commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express");
+
+
+
+const app=express();
+
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>This is an express app</h1>")
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("hello this is working");
+})
